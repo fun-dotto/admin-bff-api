4b784444e197b743f25971f30c0b19f5c0b4ddd2
Hikaru Saito <beretta0602@gmail.com>
2026-02-04 17:27:37 +0900

    Handler実装

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -4,12 +4,13 @@ import (
 	"context"
 
 	api "github.com/fun-dotto/api-template/generated"
+	"github.com/fun-dotto/api-template/internal/domain"
 )
 
 type AnnouncementService interface {
-	List(ctx context.Context) ([]api.Announcement, error)
-	Create(ctx context.Context, req *api.AnnouncementRequest) (*api.Announcement, error)
-	Update(ctx context.Context, id string, req *api.AnnouncementRequest) (*api.Announcement, error)
+	List(ctx context.Context) ([]domain.Announcement, error)
+	Create(ctx context.Context, req *domain.AnnouncementRequest) (*domain.Announcement, error)
+	Update(ctx context.Context, id string, req *domain.AnnouncementRequest) (*domain.Announcement, error)
 	Delete(ctx context.Context, id string) error
 }
 
@@ -17,7 +18,6 @@ type Handler struct {
 	announcementService AnnouncementService
 }
 
-// NewHandler 新規作成する
 func NewHandler(announcementService AnnouncementService) *Handler {
 	return &Handler{
 		announcementService: announcementService,
