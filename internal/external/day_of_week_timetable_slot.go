c6ce38cf7617352ee3a255783576983d13b7ab62
Hikaru Saito <beretta0602@gmail.com>
2026-02-16 16:10:23 +0900

    timetableの型変換

---
+++ internal/external/day_of_week_timetable_slot.go
@@ -0,0 +1,23 @@
+package external
+
+import (
+	"github.com/fun-dotto/api-template/generated/external/subject_api"
+	"github.com/fun-dotto/api-template/internal/domain"
+)
+
+// ToDomainDayOfWeekTimetableSlot 外部API形式からドメイン形式に変換する
+func ToDomainDayOfWeekTimetableSlot(d subject_api.DayOfWeekTimetableSlot) domain.DayOfWeekTimetableSlot {
+	return domain.DayOfWeekTimetableSlot{
+		ID:            d.Id,
+		DayOfWeek:     domain.DayOfWeek(d.DayOfWeek),
+		TimetableSlot: domain.TimetableSlot(d.TimetableSlot),
+	}
+}
+
+// ToExternalDayOfWeekTimetableSlotRequest ドメイン形式から外部API形式に変換する
+func ToExternalDayOfWeekTimetableSlotRequest(req *domain.DayOfWeekTimetableSlotRequest) subject_api.DayOfWeekTimetableSlotRequest {
+	return subject_api.DayOfWeekTimetableSlotRequest{
+		DayOfWeek:     subject_api.DottoFoundationV1DayOfWeek(req.DayOfWeek),
+		TimetableSlot: subject_api.DottoFoundationV1TimetableSlot(req.TimetableSlot),
+	}
+}
