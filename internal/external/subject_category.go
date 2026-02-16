49433d83bcaa7413324d394f5e1b938a0e8a060b
Hikaru Saito <beretta0602@gmail.com>
2026-02-16 16:10:49 +0900

    科目区分の型変換

---
+++ internal/external/subject_category.go
@@ -0,0 +1,21 @@
+package external
+
+import (
+	"github.com/fun-dotto/api-template/generated/external/subject_api"
+	"github.com/fun-dotto/api-template/internal/domain"
+)
+
+// ToDomainSubjectCategory 外部API形式からドメイン形式に変換する
+func ToDomainSubjectCategory(c subject_api.SubjectCategory) domain.SubjectCategory {
+	return domain.SubjectCategory{
+		ID:   c.Id,
+		Name: c.Name,
+	}
+}
+
+// ToExternalSubjectCategoryRequest ドメイン形式から外部API形式に変換する
+func ToExternalSubjectCategoryRequest(req *domain.SubjectCategoryRequest) subject_api.SubjectCategoryRequest {
+	return subject_api.SubjectCategoryRequest{
+		Name: req.Name,
+	}
+}
