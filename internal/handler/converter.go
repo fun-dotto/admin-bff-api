26184f487e1f54e3f491ec350961d71e6f48df21
Hikaru Saito <beretta0602@gmail.com>
2026-02-17 07:15:24 +0900

    facultyの変換関数を追加

--- internal/handler/converter.go
+++ internal/handler/converter.go
@@ -34,3 +34,29 @@ func ToDomainAnnouncementRequest(req *api.AnnouncementServiceAnnouncementRequest
 		AvailableUntil: req.AvailableUntil,
 	}
 }
+
+// ToAPIFaculty ドメインモデルをAPIモデルに変換する
+func ToAPIFaculty(f *domain.Faculty) api.SubjectServiceFaculty {
+	return api.SubjectServiceFaculty{
+		Id:    f.ID,
+		Name:  f.Name,
+		Email: f.Email,
+	}
+}
+
+// ToAPIFaculties ドメインモデルの配列をAPIモデルの配列に変換する
+func ToAPIFaculties(faculties []domain.Faculty) []api.SubjectServiceFaculty {
+	result := make([]api.SubjectServiceFaculty, len(faculties))
+	for i, f := range faculties {
+		result[i] = ToAPIFaculty(&f)
+	}
+	return result
+}
+
+// ToDomainFacultyRequest APIモデルをドメインモデルに変換する
+func ToDomainFacultyRequest(req *api.SubjectServiceFacultyRequest) *domain.FacultyRequest {
+	return &domain.FacultyRequest{
+		Name:  req.Name,
+		Email: req.Email,
+	}
+}
