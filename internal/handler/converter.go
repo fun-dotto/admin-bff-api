2b049dc998387b503733bae70c12ab08b3e5acef
Hikaru Saito <beretta0602@gmail.com>
2026-02-17 14:11:52 +0900

    subject_category

--- internal/handler/converter.go
+++ internal/handler/converter.go
@@ -110,3 +110,27 @@ func ToDomainDayOfWeekTimetableSlotRequest(req *api.SubjectServiceDayOfWeekTimet
 		TimetableSlot: domain.TimetableSlot(req.TimetableSlot),
 	}
 }
+
+// ToAPISubjectCategory ドメインモデルをAPIモデルに変換する
+func ToAPISubjectCategory(c *domain.SubjectCategory) api.SubjectServiceSubjectCategory {
+	return api.SubjectServiceSubjectCategory{
+		Id:   c.ID,
+		Name: c.Name,
+	}
+}
+
+// ToAPISubjectCategories ドメインモデルの配列をAPIモデルの配列に変換する
+func ToAPISubjectCategories(categories []domain.SubjectCategory) []api.SubjectServiceSubjectCategory {
+	result := make([]api.SubjectServiceSubjectCategory, len(categories))
+	for i, c := range categories {
+		result[i] = ToAPISubjectCategory(&c)
+	}
+	return result
+}
+
+// ToDomainSubjectCategoryRequest APIモデルをドメインモデルに変換する
+func ToDomainSubjectCategoryRequest(req *api.SubjectServiceSubjectCategoryRequest) *domain.SubjectCategoryRequest {
+	return &domain.SubjectCategoryRequest{
+		Name: req.Name,
+	}
+}
