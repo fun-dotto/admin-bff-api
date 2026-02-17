6eee1084f0521641f69814b6716dc4482ee684cb
Hikaru Saito <beretta0602@gmail.com>
2026-02-17 10:46:03 +0900

    timetable追加

--- internal/handler/converter.go
+++ internal/handler/converter.go
@@ -84,3 +84,29 @@ func ToDomainCourseRequest(req *api.SubjectServiceCourseRequest) *domain.CourseR
 		Name: req.Name,
 	}
 }
+
+// ToAPIDayOfWeekTimetableSlot ドメインモデルをAPIモデルに変換する
+func ToAPIDayOfWeekTimetableSlot(d *domain.DayOfWeekTimetableSlot) api.SubjectServiceDayOfWeekTimetableSlot {
+	return api.SubjectServiceDayOfWeekTimetableSlot{
+		Id:            d.ID,
+		DayOfWeek:     api.DottoFoundationV1DayOfWeek(d.DayOfWeek),
+		TimetableSlot: api.DottoFoundationV1TimetableSlot(d.TimetableSlot),
+	}
+}
+
+// ToAPIDayOfWeekTimetableSlots ドメインモデルの配列をAPIモデルの配列に変換する
+func ToAPIDayOfWeekTimetableSlots(slots []domain.DayOfWeekTimetableSlot) []api.SubjectServiceDayOfWeekTimetableSlot {
+	result := make([]api.SubjectServiceDayOfWeekTimetableSlot, len(slots))
+	for i, d := range slots {
+		result[i] = ToAPIDayOfWeekTimetableSlot(&d)
+	}
+	return result
+}
+
+// ToDomainDayOfWeekTimetableSlotRequest APIモデルをドメインモデルに変換する
+func ToDomainDayOfWeekTimetableSlotRequest(req *api.SubjectServiceDayOfWeekTimetableSlotRequest) *domain.DayOfWeekTimetableSlotRequest {
+	return &domain.DayOfWeekTimetableSlotRequest{
+		DayOfWeek:     domain.DayOfWeek(req.DayOfWeek),
+		TimetableSlot: domain.TimetableSlot(req.TimetableSlot),
+	}
+}
