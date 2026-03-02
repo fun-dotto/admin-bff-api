c70b3b727e683f92d6d9104b638f4ab65c6dd390
Hikaru Saito <beretta0602@gmail.com>
2026-03-02 14:23:52 +0900

    Handlerの初期化処理にFaculty追加

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -3,26 +3,33 @@ package handler
 import (
 	api "github.com/fun-dotto/api-template/generated"
 	"github.com/fun-dotto/api-template/generated/external/announcement_api"
+	"github.com/fun-dotto/api-template/generated/external/faculty_api"
 	"github.com/fun-dotto/api-template/generated/external/subject_api"
 )
 
 type Handler struct {
 	announcementClient *announcement_api.ClientWithResponses
+	facultyClient      *faculty_api.ClientWithResponses
 	subjectClient      *subject_api.ClientWithResponses
 }
 
 func NewHandler(
 	announcementClient *announcement_api.ClientWithResponses,
+	facultyClient *faculty_api.ClientWithResponses,
 	subjectClient *subject_api.ClientWithResponses,
 ) *Handler {
 	if announcementClient == nil {
 		panic("announcementClient is required")
+	}
+	if facultyClient == nil {
+		panic("facultyClient is required")
 	}
 	if subjectClient == nil {
 		panic("subjectClient is required")
 	}
 	return &Handler{
 		announcementClient: announcementClient,
+		facultyClient:      facultyClient,
 		subjectClient:      subjectClient,
 	}
 }
